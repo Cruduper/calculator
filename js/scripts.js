be702465b2ab0aef611cfdc01655bf2336467d05
Eric Crudup <briefcasemanx@gmail.com>
2022-01-12 22:20:20 -0800

    added fields for 4 arithmetic operations

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,52 @@
+
+
+function add(number1, number2) {
+  return number1 + number2;
+}
+
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
 $(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("div#output").children("h2").text(result);
+  });
 
+  $("form#sub").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("div#output").children("h2").text(result);
+  });
 
+  $("form#mult").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#mult1").val());
+    const number2 = parseInt($("#mult2").val());
+    const result = multiply(number1, number2);
+    $("div#output").children("h2").text(result);
+  });
+
+  $("form#divi").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divi1").val());
+    const number2 = parseInt($("#divi2").val());
+    const result = divide(number1, number2);
+    $("div#output").children("h2").text(result);
+  });
 
 });
